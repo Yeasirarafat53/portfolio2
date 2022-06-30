b3b183ff0cd71a09de01bc5fd2de1c2569be4a2c
Yeasirarafat53 <yeasirarafat1776@gmail.com>
2022-06-30 14:06:38 +0600

    scrolling button update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import ScrollToTop from 'react-scroll-to-top';
 
 const Footer = () => {
-    return (
-        <div>
-            <footer>
-    <div className="container">
-      <div className="row">
-        <div className="col-sm-12">
-          <div className="copyright-box text-center">
-            <p className="copyright">&copy; Copyright <strong>Yeasir Arafat</strong>. All Rights Reserved</p>
-
+  return (
+    <div>
+      <ScrollToTop smooth top="1000" width="20" color="white" className="scroll" />
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="copyright-box text-center">
+                <p className="copyright">
+                  &copy; Copyright <strong>Yeasir Arafat</strong>. All Rights
+                  Reserved
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </footer>
     </div>
-  </footer>
-        </div>
-    );
+  );
 };
 
 export default Footer;
